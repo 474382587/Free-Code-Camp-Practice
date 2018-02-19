5f8c6559b2eac7e11ad146958a35df59d7882170
Joseph Jin <jinyuhui61@hotmail.com>
2018-02-19 11:55:06 -0800

    s
    s

--- 284.js
+++ 284.js
@@ -1,7 +1,50 @@
 
 function updateInventory(arr1, arr2) {
     // All inventory must be accounted for or you're fired!
-    return arr1;
+  
+  for(var i = 0; i<arr1.length; i++){
+	for(var j = 0; j<arr2.length; j++){
+		if(arr1[i][1] === arr2[j][1]){
+			console.log(i+" arr1nd "+j);
+			arr1[i][0] = arr1[i][0]+arr2[j][0];
+        }
+	}
+  }
+  console.log(arr1);
+  console.log(arr2);
+  var arr = [];
+  for(var k = 0; k<arr1.length; k++){
+      arr.push(arr1[k][1]);
+  }
+  console.log("arr: " + arr);
+  var filtered = (arr2.filter(function(element){
+      return !arr.includes(element[1]); 
+
+  }));
+  console.log(filtered);
+  console.log(arr1);
+  filtered.forEach(function(element){
+
+      arr1.push(element);
+
+  });
+  var result = arr1;
+  console.log("result: " + result);
+
+  
+  result.sort(function(a,b){
+    
+    return a[1] === b[1] ? 0 : (a[1] < b[1]) ? -1 : 1;
+    
+    
+  });
+  
+  
+  
+  
+  
+  
+    return result;
 }
 
 // Example inventory lists
@@ -20,6 +63,3 @@ var newInv = [
 ];
 
 updateInventory(curInv, newInv);
-
-
-
